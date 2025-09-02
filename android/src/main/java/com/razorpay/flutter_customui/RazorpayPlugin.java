fd281d284c01569b13ef2bb0bdfaec3d422047fb
naman-jn <namanjain@ivypods.com>
2025-09-02 19:26:51 +0530

    remove unused imports

--- android/src/main/java/com/razorpay/flutter_customui/RazorpayPlugin.java
+++ android/src/main/java/com/razorpay/flutter_customui/RazorpayPlugin.java
@@ -18,9 +18,6 @@ import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 
-import io.flutter.plugin.common.PluginRegistry;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
-
 /** RazorpayFlutterCustomuiPlugin */
 
 public class RazorpayPlugin  implements FlutterPlugin, MethodCallHandler, ActivityAware , EventChannel.StreamHandler  {
